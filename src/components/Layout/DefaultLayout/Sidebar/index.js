d06a085f3ae42325fbbf5ea2f9c562d4527f7b9c
ngPoi <ngpoi1810@gmail.com>
2022-05-31 11:28:55 +0700

    split layout header, sidebar, content

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,7 +1,12 @@
+import styles from './Sidebar.module.scss'
+import classNames  from "classnames/bind"; 
+
+
+const cx = classNames.bind(styles)
 function Sidebar() {
-    return ( 
+    return <aside className={cx('wrapper')}>
         <h2>Sidebar</h2>
-     );
+    </aside>
 }
 
 export default Sidebar;
