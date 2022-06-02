9aa0a521b2b857bc33b8d6711d774ab4970260a5
ngPoi <ngpoi1810@gmail.com>
2022-06-03 00:54:27 +0700

    install font awesome

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,22 @@
-import classNames from 'classnames/bind'
-import styles from './Header.module.scss'
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+import images from '../../../../assests/images';
 
-
-const cx = classNames.bind(styles)
-
+const cx = classNames.bind(styles);
+console.log(images.logo);
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('inner')}></div>
-    </header>
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false}/>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
