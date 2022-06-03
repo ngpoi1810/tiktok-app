fbfb00f8b8d22f432fb031b61a474c2ab3bd68c9
ngPoi <ngpoi1810@gmail.com>
2022-06-03 17:04:42 +0700

    commit search bar

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,6 +1,9 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 import images from '../../../../assests/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons'
+
 
 const cx = classNames.bind(styles);
 console.log(images.logo);
@@ -12,8 +15,17 @@ function Header() {
                     <img src={images.logo} alt="Tiktok" />
                 </div>
                 <div className={cx('search')}>
-                    <input placeholder="Search accounts and videos" spellCheck={false}/>
+                    <input placeholder="Tìm kiếm tài khoản và video" spellCheck={false}/>
+                    <button className={cx('clear')}>   
+                        <FontAwesomeIcon icon={faCircleXmark} />              
+                    </button>
+                      {/* Loading */}
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
                 </div>
+                <div className={cx('actions')}></div>
             </div>
         </header>
     );
